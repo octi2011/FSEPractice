70ec1c275e08d9300e0ce508f71899aa5da2b20c
Duminica Octavian <duminicaoctavian@duminicas-macbook-pro.local>
2018-06-16 11:52:59 +0300

    Initial Commit

---
+++ FSEPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FSEPractice
+//
+//  Created by Duminica Octavian on 16/06/2018.
+//  Copyright © 2018 Duminica Octavian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
